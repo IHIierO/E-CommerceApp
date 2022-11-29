dff16b495f3d0c6dd80d5e5bbd1db92a42338fab
Artem <t-e-m-a_@mail.ru>
2022-11-29 16:36:15 +0300

    AttributedString

---
+++ E-commerceApp/ExtensionsAndHelpers/AttributedString.swift
@@ -0,0 +1,38 @@
+//
+//  AttributedString.swift
+//  E-commerceApp
+//
+//  Created by Artem Vorobev on 05.11.2022.
+//
+
+import UIKit
+
+//extension String {
+//    func createStringtToStrike(stringtToStrike: String) -> NSMutableAttributedString {
+//        let attributedString = NSMutableAttributedString(string: self)
+//        let range = attributedString.mutableString.range(of: stringtToStrike)
+//        attributedString.addAttributes([NSAttributedString.Key.strikethroughStyle : NSUnderlineStyle.single.rawValue], range: range)
+//        return attributedString
+//    }
+//    func createStringtToColor(stringtToColor: String, color: UIColor) -> NSMutableAttributedString {
+//        let attributedString = NSMutableAttributedString(string: self)
+//        let range = attributedString.mutableString.range(of: stringtToColor)
+//        attributedString.addAttributes([NSAttributedString.Key.foregroundColor : color], range: range)
+//        return attributedString
+//    }
+//}
+
+
+
+extension NSMutableAttributedString {
+    func createStringtToStrike(stringtToStrike: String){
+        //let attributedString = NSMutableAttributedString(string: self)
+        let range = self.mutableString.range(of: stringtToStrike)
+        self.addAttributes([NSAttributedString.Key.strikethroughStyle : NSUnderlineStyle.single.rawValue], range: range)
+    }
+    func createStringtToColor(stringtToColor: String, color: UIColor){
+       // let attributedString = NSMutableAttributedString(string: self)
+        let range = self.mutableString.range(of: stringtToColor)
+        self.addAttributes([NSAttributedString.Key.foregroundColor : color], range: range)
+    }
+}
